ad03690fa24eae8d463ab7bf8136457b02a93c5b
a0z9 <a0z9@rambler.ru>
2018-11-30 07:10:11 +0300

    Class commit 2 LOginPage

---
+++ src/test/java/org/andestech/learning/rfb18/g2/Login.java
@@ -0,0 +1,19 @@
+package org.andestech.learning.rfb18.g2;
+
+public class Login {
+
+    public String getLogin() {
+        return login;
+    }
+
+    private String login;
+
+    public Login(String login, String password) {
+        this.login = login;
+        this.password = password;
+    }
+
+    private String password;
+
+
+}
